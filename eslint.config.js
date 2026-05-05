9dcfe763da55a2fca4ca6de03b9a48617e3ccf02
Jake Tauer <jacobctauer@gmail.com>
2026-05-05 09:22:01 -0500

    Add ESLint (flat config), Prettier, and EditorConfig

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  {
+    ignores: ["dist/**", "node_modules/**", "coverage/**", ".superpowers/**"],
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs["eslint-recommended"].rules,
+      ...tsPlugin.configs.recommended.rules,
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/consistent-type-imports": "error",
+    },
+  },
+  prettierConfig,
+];
